53dd2a74293b2c2615bb11901db55621c6314009
Gleb Zakharov <webmaster-80@yandex.ru>
2023-04-24 19:23:17 +0300

    feat: add DispatcherServlet

--- src/main/java/ru/practicum/LaterApplication.java
+++ src/main/java/ru/practicum/LaterApplication.java
@@ -5,6 +5,7 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class LaterApplication {
     private static final int PORT = 8080;
@@ -25,11 +26,12 @@ public class LaterApplication {
         applicationContext.scan("ru.practicum");
         applicationContext.refresh();
 
-        // класс Wrapper позволяет задать дополнительные настройки для сервлета
-        Wrapper testServletWrapper =
-                Tomcat.addServlet(tomcatContext, "testServlet", new TestServlet());
-        // addMapping() сопоставляет URL-путь с сервлетом
-        testServletWrapper.addMapping("/test");
+        // добавляем диспетчер запросов
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+        Wrapper dispatcherWrapper = Tomcat.addServlet(tomcatContext, "dispatcher", dispatcherServlet);
+        dispatcherWrapper.addMapping("/");
+        dispatcherWrapper.setLoadOnStartup(1);
+
         tomcat.start();
     }
 }
