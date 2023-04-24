44a79350b6ed8de7f7753483323f4310d25bc2a9
Gleb Zakharov <webmaster-80@yandex.ru>
2023-04-24 19:21:16 +0300

    feat: add WebApplicationContext

--- src/main/java/ru/practicum/LaterApplication.java
+++ src/main/java/ru/practicum/LaterApplication.java
@@ -4,6 +4,7 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 public class LaterApplication {
     private static final int PORT = 8080;
@@ -16,6 +17,13 @@ public class LaterApplication {
 
         // то самое "приложение" или "контекст" с пустым путём
         Context tomcatContext = tomcat.addContext("", null);
+
+        // создаём контекст
+        AnnotationConfigWebApplicationContext applicationContext =
+                new AnnotationConfigWebApplicationContext();
+        applicationContext.setServletContext(tomcatContext.getServletContext());
+        applicationContext.scan("ru.practicum");
+        applicationContext.refresh();
 
         // класс Wrapper позволяет задать дополнительные настройки для сервлета
         Wrapper testServletWrapper =
