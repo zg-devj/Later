01421dd59c5611078fc49a802ed3ec4339be8604
Gleb Zakharov <webmaster-80@yandex.ru>
2023-04-24 19:29:44 +0300

    feat: add TestControllerontroller

--- src/main/java/ru/practicum/LaterApplication.java
+++ src/main/java/ru/practicum/LaterApplication.java
@@ -4,24 +4,12 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class LaterApplication {
-    private static final int PORT = 8080;
-
     public static void main(String[] args) throws LifecycleException {
-        Tomcat tomcat = new Tomcat();
-
-        // connector -- это компонент, который отвечает за "сеть"
-        tomcat.getConnector().setPort(PORT);
-
-        // то самое "приложение" или "контекст" с пустым путём
-        Context tomcatContext = tomcat.addContext("", null);
-
-        // класс Wrapper позволяет задать дополнительные настройки для сервлета
-        Wrapper testServletWrapper =
-                Tomcat.addServlet(tomcatContext, "testServlet", new TestServlet());
-        // addMapping() сопоставляет URL-путь с сервлетом
-        testServletWrapper.addMapping("/test");
-        tomcat.start();
+        SpringApplication.run(LaterApplication.class, args);
     }
 }
