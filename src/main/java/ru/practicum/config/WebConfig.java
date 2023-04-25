54370b5c50210d70fac2c9d90a7e5a7a3fb3e8fa
Gleb Zakharov <webmaster-80@yandex.ru>
2023-04-25 08:20:07 +0300

    feat: add to config with PropertySource

--- src/main/java/ru/practicum/config/WebConfig.java
+++ src/main/java/ru/practicum/config/WebConfig.java
@@ -1,9 +1,13 @@
 package ru.practicum.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration // помечает класс как java-config для контекста приложения
 @EnableWebMvc // призывает импортировать дополнительную конфигурацию для веб-приложений
+@PropertySource("classpath:application.properties")
+//@PropertySource(value = "file:C:\\myapp\\myapp.properties", ignoreResourceNotFound = true) // пример для Windows
+//@PropertySource(value = "file:/etc/myapp/myapp.properties", ignoreResourceNotFound = true) // пример для Unix-like
 public class WebConfig {
 }
