d037fe0703f8fb350bd38085bc8cae2aa34a71eb
Gleb Zakharov <webmaster-80@yandex.ru>
2023-05-05 15:38:01 +0300

    feat: added dependencies jpa, hibernate, postgresql

--- src/main/java/ru/practicum/config/WebConfig.java
+++ src/main/java/ru/practicum/config/WebConfig.java
@@ -5,5 +5,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration // помечает класс как java-config для контекста приложения
 @EnableWebMvc // призывает импортировать дополнительную конфигурацию для веб-приложений
+//@PropertySource(value = "file:C:\\myapp\\myapp.properties", ignoreResourceNotFound = true) // пример для Windows
+//@PropertySource(value = "file:/etc/myapp/myapp.properties", ignoreResourceNotFound = true) // пример для Unix-like
 public class WebConfig {
 }
