92db628093ac3aeb70f562b328833b6e1853e768
Gleb Zakharov <webmaster-80@yandex.ru>
2023-05-06 08:19:44 +0300

    feat: added entity to Item.java

--- src/main/java/ru/practicum/item/Item.java
+++ src/main/java/ru/practicum/item/Item.java
@@ -2,9 +2,21 @@ package ru.practicum.item;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
+@Entity
+@Table(name = "items", schema = "public")
 public class Item {
+    @Id
     private Long id;
+
+    @Column(name = "user_id")
     private Long userId;
+
+    @Column
     private String url;
 }
--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -2,9 +2,27 @@ package ru.practicum.user;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.time.Instant;
+
 @Data
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
+    @Id
     private Long id;
+
     private String email;
-    private String name;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "registration_date")
+    private Instant registrationDate;
+
+    @Enumerated(EnumType.STRING)
+    private UserState state;
 }
