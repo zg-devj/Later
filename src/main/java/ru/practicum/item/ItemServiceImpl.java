737dde63e62a473d92a57ed6723a47e13b93808e
Gleb Zakharov <webmaster-80@yandex.ru>
2023-05-10 14:14:25 +0300

    feat: added transaction

--- src/main/java/ru/practicum/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/item/ItemServiceImpl.java
@@ -2,10 +2,12 @@ package ru.practicum.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
 
@@ -16,12 +18,14 @@ public class ItemServiceImpl implements ItemService {
         return itemRepository.findByUserId(userId);
     }
 
+    @Transactional
     @Override
     public Item addNewItem(Long userId, Item item) {
         item.setUserId(userId);
         return itemRepository.save(item);
     }
 
+    @Transactional
     @Override
     public void deleteItem(Long userId, long itemId) {
         itemRepository.deleteByUserIdAndItemId(userId, itemId);
--- src/main/java/ru/practicum/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/user/UserServiceImpl.java
@@ -2,10 +2,12 @@ package ru.practicum.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository repository;
@@ -16,6 +18,7 @@ public class UserServiceImpl implements UserService {
         return repository.findAll();
     }
 
+    @Transactional
     @Override
     public User saveUser(User user) {
         return repository.save(user);
