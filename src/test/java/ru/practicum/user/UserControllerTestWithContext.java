9ca6425df644e0f91f745b05518275f40e969288
Gleb <webmaster-80@yandex.ru>
2025-05-20 17:53:19 +0300

    feat: add tests

--- src/test/java/ru/practicum/user/UserControllerTestWithContext.java
+++ src/test/java/ru/practicum/user/UserControllerTestWithContext.java
@@ -1,14 +1,12 @@
 package ru.practicum.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.StandardCharsets;
 
@@ -19,50 +17,40 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest({UserController.class})
+@WebMvcTest(controllers = UserController.class)
 class UserControllerTestWithContext {
-//    private final ObjectMapper mapper = new ObjectMapper();
-//
-//    private final UserService userService;
-//
-//    private MockMvc mvc;
-//
-//    private UserDto userDto;
-//
-//    @Autowired
-//    UserControllerTestWithContext(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @BeforeEach
-//    void setUp(WebApplicationContext wac) {
-//        mvc = MockMvcBuilders
-//                .webAppContextSetup(wac)
-//                .build();
-//
-//        userDto = new UserDto(
-//                1L,
-//                "john.doe@mail.com",
-//                "John",
-//                "Doe",
-//                "2022.07.03 19:55:00",
-//                UserState.ACTIVE);
-//    }
-//
-//    @Test
-//    void saveNewUser() throws Exception {
-//        when(userService.saveUser(any()))
-//                .thenReturn(userDto);
-//
-//        mvc.perform(post("/users")
-//                        .content(mapper.writeValueAsString(userDto))
-//                        .characterEncoding(StandardCharsets.UTF_8)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
-//                .andExpect(jsonPath("$.firstName", is(userDto.getFirstName())))
-//                .andExpect(jsonPath("$.lastName", is(userDto.getLastName())))
-//                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
-//    }
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private UserDto userDto = new UserDto(
+            1L,
+            "john.doe@mail.com",
+            "John",
+            "Doe",
+            "2022.07.03 19:55:00",
+            UserState.ACTIVE);
+
+    @Test
+    void saveNewUser() throws Exception {
+        when(userService.saveUser(any()))
+                .thenReturn(userDto);
+
+        mvc.perform(post("/users")
+                        .content(mapper.writeValueAsString(userDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
+                .andExpect(jsonPath("$.firstName", is(userDto.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(userDto.getLastName())))
+                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+    }
 }
---
+++ src/test/java/ru/practicum/user/UserDtoJsonTest.java
@@ -0,0 +1,33 @@
+package ru.practicum.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class UserDtoJsonTest {
+    @Autowired
+    private JacksonTester<UserDto> json;
+
+    @Test
+    void testUserDto() throws Exception {
+        UserDto userDto = new UserDto(
+                1L,
+                "john.doe@mail.com",
+                "John",
+                "Doe",
+                "2022.07.03 19:55:00",
+                UserState.ACTIVE);
+
+        JsonContent<UserDto> result = json.write(userDto);
+
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathStringValue("$.firstName").isEqualTo("John");
+        assertThat(result).extractingJsonPathStringValue("$.lastName").isEqualTo("Doe");
+        assertThat(result).extractingJsonPathStringValue("$.email").isEqualTo("john.doe@mail.com");
+    }
+}
